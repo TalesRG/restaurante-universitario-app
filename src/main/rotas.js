1c7bc29bfa0a355f3731cb95c1d58958a500bf08
JoaoBarreto03 <108282056+JoaoBarreto03@users.noreply.github.com>
2022-08-17 13:57:15 -0300

    adicionando tela HOME

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -2,15 +2,18 @@ import React from "react";
 import Login from "../views/login";
 import CadastroUsuario from "../views/cadastroUsuario";
 import Home from "../views/home";
+import RestauranteRecarga from "../views/restaurante-recarga";
 import {Route,Switch,HashRouter} from 'react-router-dom'
+
 
 function Rotas(){
     return(
         <HashRouter>
             <Switch>
                 <Route path= "/login" component={Login}></Route>
-                <Route path= "/cadastro-usuario" component={CadastroUsuario}></Route>
                 <Route path= "/home" component={Home}></Route>
+                <Route path= "/cadastro-usuario" component={CadastroUsuario}></Route>
+                <Route path= "/ru-recarga" component={RestauranteRecarga}></Route>
             </Switch>
         </HashRouter>
     )
--- src/views/home.js
+++ src/views/home.js
@@ -1,23 +1,85 @@
 import React from "react";
-import Card from "../componentes/card";
-import FormGroup from "../componentes/form-group";
-import {withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'; 
+import { Carousel } from 'react-carousel-minimal';
+import CardHome from '../componentes/cardHome';
+import '../cardHome.css';
+
 class Home extends React.Component{
-    render(){
-        return (
-            <Card title ="Cadatro de Usuário">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="bs-componet">
-                            <FormGroup label ="Nome * " htmlFor="inputNome">
-                            </FormGroup>
-                        </div>
-                    </div>
-                </div>
-            </Card>
-    
-        )
+    entrar = () => {
+        this.props.history.push('/login')
     }
+    prepararCadastrar = () =>{
+        this.props.history.push('/cadastro-usuario')
+    }
+    render(){
+                
+             const data = [
+                {
+                  image: "https://i.imgur.com/4NGFdgv.jpg",
+                  caption: "Faça o controle de suas recargas"
+                                         
+                },
+                {
+                  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Campus_da_UnB_no_Gama_%28FGA%29_completa_10_anos_%2839135873690%29.jpg/800px-Campus_da_UnB_no_Gama_%28FGA%29_completa_10_anos_%2839135873690%29.jpg",
+                  caption: "Entre ou faça seu cadastro gratuitamente"
+                }
+
+                
+              ];
+            
+              const captionStyle = {
+                fontSize: '2em',
+                fontWeight: 'bold',
+              }
+              const slideNumberStyle = {
+                fontSize: '20px',
+                fontWeight: 'bold',
+              }
+        return (
+                <div className="App">
+                
+                  <div style={{ textAlign: "center" }}>
+                    <h1>Bem-vindo ao Restaurante Universitário!</h1>
+
+                    <p>Fique atualizado com o cardápio da semana e gerencie seus gastos</p>
+                    <div style={{
+                      padding: "0 20px"
+                    }}>
+                      <Carousel
+                        data={data}
+                        time={2000}
+                        width="1800px"
+                        height="500px"
+                        captionStyle={captionStyle}
+                        radius="10px"
+                        slideNumber={false}
+                        slideNumberStyle={slideNumberStyle}
+                        captionPosition="bottom"
+                        automatic={true}
+                        dots={false}
+                        pauseIconColor="blue"
+                        pauseIconSize="40px"
+                        slideBackgroundColor="black"
+                        slideImageFit="cover"
+                        thumbnails={false}
+                        thumbnailWidth="100px"
+                        style={{
+                          textAlign: "center",
+                          maxWidth: "1920px",
+                          maxHeight: "1080px",
+                          margin: "40px auto",
+                          
+                        }}
+                      />
+                    </div>
+                  </div>
+                       <CardHome title="Login">
+                
+                
+                          </CardHome>
+                      </div>
     
+              );
+    }
 }
 export default withRouter(Home)
